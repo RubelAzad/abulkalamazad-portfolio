d95995c45371482619ad13adc5e5c5d5f7ff2c7a
RubelAzad <rubelazad123@gmail.com>
2024-07-01 15:53:45 +0600

    update

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -5,7 +5,7 @@ function Loader() {
     <div className="h-screen flex items-center justify-center fixed inset-0 bg-primary z-[10000]">
       <div className="flex gap-5 text-6xl font-semibold sm:text-3xl">
         <h1 className="m" style={{ color: "#48a1d4" }}>
-          A
+          A 
         </h1>
         <h1 className="d" style={{ color: "#118bd1" }}>
           K
